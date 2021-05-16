8cb01e9a917345807a89e504426b71dd941ce35d
Philip <psnell63@gmail.com>
2021-05-16 09:34:53 +0100

    static files

--- xcell/settings.py
+++ xcell/settings.py
@@ -134,7 +134,7 @@ AUTH_USER_MODEL = 'api.Account'
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 )
 
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media/')
