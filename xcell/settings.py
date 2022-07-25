09540dd74501ef1c4266b54bce7755f1aa8ee7a1
Phil <psnell63@gmail.com>
2022-07-24 20:41:15 -0700

    added email password reset

--- xcell/settings.py
+++ xcell/settings.py
@@ -129,6 +129,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = "peaktrainingplatform@gmail.com"
+EMAIL_HOST_PASSWORD = "lgksybtlhuaccyfb"
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
